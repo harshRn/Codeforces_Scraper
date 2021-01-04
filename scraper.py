ca8d94e4cf652fda220e928b03f97fe8ef2e7fda
Harsh Ranjan <harsh150997@gmail.com>
2021-01-05 02:01:59 +0530

    finalized scraper

--- scraper.py
+++ scraper.py
@@ -2,45 +2,40 @@ from bs4 import BeautifulSoup
 import requests
 
 
-source=requests.get("https://codeforces.com/problemset").text
+#the engine
+def process(link,a,b):
+    source=requests.get(link).text
+    soup=BeautifulSoup(source,'lxml')
 
+    problems=soup.find('table',class_='problems')
+    rows=problems.find_all(['tr'])
 
-soup=BeautifulSoup(source,'lxml')
+    lst=[]
 
-problems=soup.find('table',class_='problems')
-
-rows=problems.find_all(['tr'])
-
-
-a=int(input())
-b=int(input())
-
-lst=[]
-
-for i in rows:
-    id=i.find_all('a')
+    for i in rows:
+        lst2=[]
+        id=i.find_all('a')
     
-    code=id[0].text.strip()
-    name=id[1].text.strip()
+        code=id[0].text.strip()
+        name=id[1].text.strip()
     
-    link="www.codeforces.com"
-    link+=id[0]['href']
+        link="www.codeforces.com"
+        link+=id[0]['href']
     
-    diff=i.find('span',class_='ProblemRating')
-    if(diff!=None):
-        diff=diff.text
-        diff=int(diff)
-    else:
-        continue
+        diff=i.find('span',class_='ProblemRating')
+        if(diff!=None):
+            diff=diff.text
+            diff=int(diff)
+        else:
+            continue
 
-    if(diff>=a and diff<=b):
-        lst.append(code)
-        lst.append(name)
-        lst.append(diff)
-        lst.append(link)
-print(len(lst))
-
-   
-
-
-
+        if(diff>=a and diff<=b):
+            lst2.append(code)
+            lst2.append(name)
+            lst2.append(diff)
+            lst2.append(link)
+        else:
+            continue
+        lst.append(lst2)
+    print(len(lst))
+    return lst2;
